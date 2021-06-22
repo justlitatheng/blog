0b8b16afda6120aacaee237df21be5fa73c2db7b
justlitatheng <56949321+justlitatheng@users.noreply.github.com>
2021-06-22 16:04:13 -0400

    issue: site break after entering a post
    there is a problem when I go to localhost:3000/posts and I click on one of the blog post, the site crashes.

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,44 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
+
+import '../stylesheets/application';
+
+require("trix")
+require("@rails/actiontext")
+
+
+document.addEventListener('turbolinks:load', () => {
+    document.addEventListener('click', () => {
+        let element = event.target.closest('.paragraph-content')
+        if (!element) return;
+
+        element.classList.add('d-none')
+        element.nextElementSibling.classList.remove('d-none')
+    })
+
+    document.addEventListener('click', () => {
+        if (!event.target.matches('.cancel')) return;
+
+        let element = event.target.closest('.paragraph-form')
+
+        element.classList.add('d-none')
+        element.previousElementSibling.classList.remove('d-none')
+    })
+})
+import "controllers"
